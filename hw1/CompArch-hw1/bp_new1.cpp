94133da392af0ad19a77880f5e7e35c1e93dc96d
On Fabian <ononon1993@gmail.com>
2023-05-04 15:36:21 +0300

    works

--- hw1/CompArch-hw1/bp_new1.cpp
+++ hw1/CompArch-hw1/bp_new1.cpp
@@ -16,24 +16,24 @@ const int NOT_USING_SHARE = 0;
 const int USING_SHARE_LSB = 1;
 const int USING_SHARE_MID = 2;
  
-
-class FsmBimodal {
+ 
+class fsm_2bit {
 	private:
-	unsigned curr_state;
+		unsigned curr_state;
 
 	public:
-	FsmBimodal(): curr_state(0) {}
-	~FsmBimodal() = default;
-	int get_state();
-	void move_state(bool t);
-	void set_state(unsigned _curr_state);
+		fsm_2bit(): curr_state(0) {}
+		~fsm_2bit() = default;
+		int get_state();
+		void move_state(bool t);
+		void set_state(unsigned curr_state_tmp);
 };
 
-int FsmBimodal:: get_state() {
+int fsm_2bit::get_state() {
 	return curr_state;
 }
 
-void FsmBimodal::move_state(bool t) {
+void fsm_2bit::move_state(bool t) {
 	if ((t==true) && (curr_state != ST)) {
 		curr_state++;
 	} else if ((t==false) && (curr_state != SNT)) {
@@ -41,8 +41,8 @@ void FsmBimodal::move_state(bool t) {
 	}
 }
 
-void FsmBimodal::set_state(unsigned _curr_state) {
-	curr_state = _curr_state;
+void fsm_2bit::set_state(unsigned curr_state_tmp) {
+	curr_state = curr_state_tmp;
 } 
 
 class BP_row {
@@ -55,16 +55,16 @@ class BP_row {
 		unsigned fsmState;
 		int history_size;
 		uint32_t history;
-		std::vector<FsmBimodal> fsms;
+		std::vector<fsm_2bit> fsms;
 	public:
 		static uint32_t global_history; 
-		static vector<FsmBimodal> global_fsms;
-		BP_row(uint32_t _tag, uint32_t _target, bool _isGlobalHist, bool _isGlobalTable, int _shared, unsigned _fsmState, int _historySize);
+		static vector<fsm_2bit> global_fsms;
+		BP_row(uint32_t tmp_tag, uint32_t tmp_target, bool tmp_isGlobalHist, bool tmp_isGlobalTable, int tmp_isGlobalTable, unsigned tmp_fsmState, int tmp_historySize);
 		~BP_row() = default;
 		
-		uint32_t get_tag(); 
+		uint32_t gettmp_tag(); 
 
-		void updateTarget(uint32_t _target);
+		void updateTarget(uint32_t tmp_target);
 
 		void changeHistory(bool decision);
 
@@ -73,15 +73,15 @@ class BP_row {
 		bool getFSMstate(uint32_t pc, uint32_t *dst);
 };
 
-BP_row::BP_row(uint32_t _tag, uint32_t _target, bool _isGlobalHist, bool _isGlobalTable, int _shared, unsigned _fsmState, int _historySize) {
-	tag = _tag;
-	target = _target;
-	isGlobalHist = _isGlobalHist;
-	isGlobalTable = _isGlobalTable;
-	shared = _shared;
-	fsmState = _fsmState;
-	history_size = _historySize;
-	fsms = vector<FsmBimodal>(int(pow(2, _historySize)));
+BP_row::BP_row(uint32_t tmp_tag, uint32_t tmp_target, bool tmp_isGlobalHist, bool tmp_isGlobalTable, int tmp_isGlobalTable, unsigned tmp_fsmState, int tmp_historySize) {
+	tag = tmp_tag;
+	target = tmp_target;
+	isGlobalHist = tmp_isGlobalHist;
+	isGlobalTable = tmp_isGlobalTable;
+	shared = tmp_isGlobalTable;
+	fsmState = tmp_fsmState;
+	history_size = tmp_historySize;
+	fsms = vector<fsm_2bit>(int(pow(2, tmp_historySize)));
 
 	if (isGlobalHist) {
 		history = 0;
@@ -89,27 +89,27 @@ BP_row::BP_row(uint32_t _tag, uint32_t _target, bool _isGlobalHist, bool _isGlob
 	if (isGlobalTable) {
 		fsms.clear();
 	}else {
-		for(vector<FsmBimodal>::iterator it = fsms.begin(); it != fsms.end(); ++it) {
+		for(vector<fsm_2bit>::iterator it = fsms.begin(); it != fsms.end(); ++it) {
 			it->set_state(fsmState);
 		}
 	}
 }
 
-uint32_t BP_row::get_tag() {
+uint32_t BP_row::gettmp_tag() {
 	return this->tag;
 }
 
-void BP_row::updateTarget(uint32_t _target) {
-	target = _target;
+void BP_row::updateTarget(uint32_t tmp_target) {
+	target = tmp_target;
 }
 
 void BP_row::changeHistory(bool decision) {
 	if (isGlobalHist) {
-		global_history = (global_history << 1) | decision;
-		global_history &= (1 << history_size) - 1;
+		global_history = (global_history*2) | decision;
+		global_history &= (pow(2,history_size)) - 1;
 	    } else {
-		history = (history << 1) | decision;
-		history &= (1 << history_size) - 1;
+		history = (history*2) | decision;
+		history &= (global_history*2) - 1;
 	    }
 }
 
@@ -121,29 +121,29 @@ void BP_row::changeFSMState(bool decision, uint32_t pc) {
     		if (shared == NOT_USING_SHARE) {
                 index = global_history;
         } else if (shared == USING_SHARE_LSB) {
-		pc_masked = pc >> 2;
+		pc_masked = pc/pow(2,2);
         } else if (shared == USING_SHARE_MID) {
-	        pc_masked = pc >> 16;
+	        pc_masked = pc/pow(2,16);
         }
 	index = pc_masked ^ global_history;
-        index &= (1 << history_size) - 1;
+        index &= (pow(2,history_size)) - 1;
         global_fsms[index].move_state(decision);
     } else if (isGlobalTable) {
         if (shared == NOT_USING_SHARE) {
      	       index = history;
         } else if (shared == USING_SHARE_LSB) {
-               pc_masked = pc >> 2;
+               pc_masked = pc/pow(2,2);
         } else if (shared == USING_SHARE_MID) {
-               pc_masked = pc >> 16;
+               pc_masked = pc/pow(2,16);
         }
 	index = pc_masked ^ history;
-        index &= (1 << history_size) - 1;
+        index &= (pow(2,history_size)) - 1;
         global_fsms[index].move_state(decision);
     } else if (isGlobalHist) {
-	index = global_history & ((1 << history_size) - 1);
+	index = global_history & ((pow(2,history_size)) - 1);
         fsms[index].move_state(decision);
     } else {
-        index = history & ((1 << history_size) - 1);
+        index = history & ((pow(2,history_size)) - 1);
         fsms[index].move_state(decision);
     }
 }
@@ -156,24 +156,24 @@ bool BP_row::getFSMstate(uint32_t pc, uint32_t *dst) {
 
 		case NOT_USING_SHARE: 
 		    if (isGlobalHist && isGlobalTable) {
-		    	index = global_history & ((1 << history_size) - 1);
+		    	index = global_history & ((pow(2,history_size)) - 1);
 		    } 
 		    else if (isGlobalTable) {
-		    	index = history & ((1 << history_size) - 1);
+		    	index = history & ((pow(2,history_size)) - 1);
 		    } 
 		    else {
-		    	index = history & ((1 << history_size) - 1);
+		    	index = history & ((pow(2,history_size)) - 1);
 		    }
 		    break;
 
 		case USING_SHARE_LSB:
 		    historyValue = isGlobalHist ? global_history : history;
-		    index = (((pc >> 2) ^ historyValue) & ((1 << history_size) - 1));
+		    index = (((pc/pow(2,2)) ^ historyValue) & ((pow(2,history_size)) - 1));
 		    break;
 
 		case USING_SHARE_MID:
 		    historyValue = isGlobalHist ? global_history : history;
-		    index = (((pc >> 16) ^ historyValue) & ((1 << history_size) - 1));
+		    index = (((pc/pow(2,16)) ^ historyValue) & ((pow(2,history_size)) - 1));
 		    break;
 
 		default:
@@ -181,7 +181,7 @@ bool BP_row::getFSMstate(uint32_t pc, uint32_t *dst) {
 		    return false;
 	}
 
-	if (index >= 0 && index < (1 << history_size)) {
+	if (index >= 0 && index < (pow(2,history_size))) {
 		if (global_fsms[index].get_state() & 2) {
 		    *(dst) = target; 
 		    return true;
@@ -190,7 +190,7 @@ bool BP_row::getFSMstate(uint32_t pc, uint32_t *dst) {
 		    *(dst) = pc + 4;
 		    return false;
 		}
-	} else if (index >= 0 && index < (1 << history_size)) {
+	} else if (index >= 0 && index < (pow(2,history_size))) {
 	 	if (fsms[index].get_state() & 2) {
 		    *(dst) = target; 
 		    return true;
@@ -217,20 +217,20 @@ class BP {
 		bool isGlobalHist;
 		bool isGlobalTable; 
 		int Shared;
-		unsigned num_of_flushes;
-		unsigned num_of_updates;
+		unsigned flushes_counter;
+		unsigned updates_counter;
 		vector<BP_row*> btbTable;
 
-		BP(unsigned _btbSize, unsigned _historySize, unsigned _tagSize, unsigned _fsmState, bool _isGlobalHist, bool _isGlobalTable, int _Shared):
+		BP(unsigned _btbSize, unsigned tmp_historySize, unsigned tmp_tagSize, unsigned tmp_fsmState, bool tmp_isGlobalHist, bool tmp_isGlobalTable, int tmp_isGlobalTable):
 			btbSize(_btbSize), 
-			historySize(_historySize),
-			tagSize(_tagSize),
-			fsmState(_fsmState),
-			isGlobalHist(_isGlobalHist),
-			isGlobalTable(_isGlobalTable),
-			Shared(_Shared),
-			num_of_flushes(0),
-			num_of_updates(0),
+			historySize(tmp_historySize),
+			tagSize(tmp_tagSize),
+			fsmState(tmp_fsmState),
+			isGlobalHist(tmp_isGlobalHist),
+			isGlobalTable(tmp_isGlobalTable),
+			Shared(tmp_isGlobalTable),
+			flushes_counter(0),
+			updates_counter(0),
 			btbTable(std::vector<BP_row*>(btbSize))	{}
 
 		~BP() {
@@ -243,7 +243,7 @@ class BP {
 
 		int index_size(unsigned btbSize);
 
-		bool getTagFromBTB(uint32_t pc, uint32_t* _pc_tag, uint32_t* _btb_index, uint32_t* _tag_in_btb);
+		bool getTagFromBTB(uint32_t pc, uint32_t* tmp_pcTag, uint32_t* tmp_btb_index, uint32_t* tmp_tag_in_btb);
 
 };
 
@@ -251,16 +251,16 @@ int BP::index_size(unsigned btbSize) {
 	return int(floor(log2(btbSize)));
 }
 
-bool BP::getTagFromBTB(uint32_t pc, uint32_t* _pc_tag, uint32_t* _btb_index, uint32_t* _tag_in_btb) {
+bool BP::getTagFromBTB(uint32_t pc, uint32_t* tmp_pcTag, uint32_t* tmp_btb_index, uint32_t* tmp_tag_in_btb) {
 	uint32_t btb_index = pc >> 2; 
-	uint32_t pc_tag = btb_index; 
+	uint32_t pctmp_tag = btb_index; 
 	int btb_index_size = index_size(btbSize);
-	pc_tag = (pc_tag >> btb_index_size) & ((1 << tagSize) - 1);
-	btb_index = btb_index & ((1 << btb_index_size) - 1);
-	*_btb_index = btb_index;
-	*_pc_tag = pc_tag;
+	pctmp_tag = (pctmp_tag >> btb_index_size) & ((1 << tagSize) - 1);
+	btb_index = btb_index & ((pow(2,btb_index_size)) - 1);
+	*tmp_btb_index = btb_index;
+	*tmp_pcTag = pctmp_tag;
 	if (btbTable[btb_index]) {
-		*_tag_in_btb = btbTable[btb_index]->get_tag();
+		*tmp_tag_in_btb = btbTable[btb_index]->gettmp_tag();
 		return true;
 	}
 	return false;
@@ -268,7 +268,7 @@ bool BP::getTagFromBTB(uint32_t pc, uint32_t* _pc_tag, uint32_t* _btb_index, uin
 
 
 BP* bp;
-vector<FsmBimodal> BP_row::global_fsms = vector<FsmBimodal>(0);
+vector<fsm_2bit> BP_row::global_fsms = vector<fsm_2bit>(0);
 uint32_t BP_row::global_history = 0;
 
 
@@ -279,7 +279,7 @@ int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned f
 			return -1;
 		}
 		BP_row::global_fsms.resize(int(pow(2, historySize)));
-		for(vector<FsmBimodal>::iterator it = BP_row::global_fsms.begin(); it != BP_row::global_fsms.end(); ++it) {
+		for(vector<fsm_2bit>::iterator it = BP_row::global_fsms.begin(); it != BP_row::global_fsms.end(); ++it) {
 			it->set_state(fsmState);
 		}
 		return 0;
@@ -321,17 +321,17 @@ void updateExistingLine(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pre
 	bp->btbTable[*btb_index]->changeHistory(taken);
 	
 	if (((targetPc != pred_dst) && (taken)) || ( (pred_dst != (pc + 4) ) && (!taken) ) ) {
-		bp->num_of_flushes++;
+		bp->flushes_counter++;
 	}
 }
 
-void createNewLine(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst, uint32_t* pc_tag, uint32_t* btb_index) {
-	bp->btbTable[*btb_index] = new BP_row(*pc_tag, targetPc, bp->isGlobalHist, bp->isGlobalTable, bp->Shared, bp->fsmState, bp->historySize);
+void createNewLine(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst, uint32_t* pctmp_tag, uint32_t* btb_index) {
+	bp->btbTable[*btb_index] = new BP_row(*pctmp_tag, targetPc, bp->isGlobalHist, bp->isGlobalTable, bp->Shared, bp->fsmState, bp->historySize);
 	bp->btbTable[*btb_index]->changeFSMState(taken, pc);
 	bp->btbTable[*btb_index]->changeHistory(taken);
 	
 	if (taken && (targetPc != pc + 4) && (targetPc != pred_dst)) {
-		bp->num_of_flushes++;
+		bp->flushes_counter++;
 	}
 }
 
@@ -346,12 +346,12 @@ void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst) {
         createNewLine(pc, targetPc, taken, pred_dst, &tag, &btb_index);
     }
     
-    bp->num_of_updates++;
+    bp->updates_counter++;
 }
 
 void BP_GetStats(SIM_stats *curStats) {
-	curStats->br_num = bp->num_of_updates;
-	curStats->flush_num = bp->num_of_flushes;
+	curStats->br_num = bp->updates_counter;
+	curStats->flush_num = bp->flushes_counter;
 	int btb_size_term = bp->btbSize * (1 + bp->tagSize + 30);
 	int history_size_term = 2 * (int(pow(2, bp->historySize)));
 	if (bp->isGlobalTable && bp->isGlobalHist) {
